24ad616a958ccaa058f59c7bbc88a05bce595afa
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-28 16:05:06 -0300

    atualização

--- automacao.py
+++ automacao.py
@@ -1,1 +1,9 @@
 import streamlit as st
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+#https://arthurresendes.github.io/supplier_automation/chamado.html
+
+def stream():
+    st.title("Bem vindos ao Supplier Automation")
+    st.subheader("Nele será possivel ver um exemplo de preenchimento de formulario pegando dados de uma planilha e pegando a requisição gerada direto para a sua planilha!!")
+    upload = st.file_uploader("Escolha seu arquivo XLSX: ", type=['xlsx','xls'])
