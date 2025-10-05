42dfd825dc7b5ee9f3c1bef813565c1e4020c6e9
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-05 13:45:25 -0300

    atualização

--- criacao_banco.py
+++ criacao_banco.py
@@ -1,21 +1,21 @@
 import sqlite3
 
-conexao = sqlite3.connect("supplier.db")
+def criacao():
+    conexao = sqlite3.connect("supplier.db")
+    
+    cursor = conexao.cursor()
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS USER (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nome VARCHAR(200),
+                    admin VARCHAR(200),
+                    matricula VARCHAR(10),
+                    valor REAL,
+                    RITM VARCHAR(15)
+                );
+    ''')
+    conexao.commit()
+    conexao.close()
 
-cursor = conexao.cursor()
-
-
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS USER (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome VARCHAR(200),
-        admin VARCHAR(200),
-        matricula VARCHAR(10),
-        valor REAL,
-        RITM VARCHAR(15)
-    );
-''')
-
-conexao.commit()
-
-conexao.close()
+if __name__ == "__main__":
+    criacao()
